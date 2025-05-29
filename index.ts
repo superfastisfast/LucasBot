1c471e9b1d404fb188b93deab6de87fac7f83475
root <root@DESKTOP-659ES3P.localdomain>
2025-05-29 21:24:22 +0300

    cool Jasper show commands

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, Events } from "discord.js";
+import { Client, GatewayIntentBits, Events, Message } from "discord.js";
 
 const client = new Client({
     intents: [
@@ -9,12 +9,61 @@ const client = new Client({
     ]
 });
 
+
+
+class Command {
+    protected name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    public async execute(message: Message, args: string[]): Promise<void> { }
+    public getName(): string {
+        return this.name;
+    }
+}
+
+
+
+
+
+class PingCommand extends Command {
+    constructor() {
+        super("ping");
+    }
+
+    public override async execute(message: Message, args: string[]): Promise<void> {
+        await message.reply("Pong!");
+        console.log("Ping executed!", args);
+    }
+}
+
+
+const commands: Map<string, Command> = new Map();
+commands.set("ping", new PingCommand());
+commands.set("uwu", new PingCommand());
+
+
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
-    client.on(Events.MessageCreate, msg => {
-        if (msg.content === "peng")
-            msg.reply("pang");
+    client.on(Events.MessageCreate, async (msg) => {
+
+        const { content } = msg;
+
+        //!ping
+        //bun run --watch index.ts
+
+        if (content.startsWith("!")) {
+            const args = content.slice(1).trim().split(/ +/);
+            const commandName = args.shift().toLowerCase();
+
+            const command = commands.get(commandName);
+            if (command) {
+                await command.execute(msg, args)
+            }
+        }
+
         console.log(msg.content);
     });
 
