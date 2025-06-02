66e7d086c5ab8a0a8209776ef6ae3cbe43910bc8
HaraldWik <wikhallharaldl@gmail.com>
2025-06-02 19:14:07 +0200

    Improved XP command pun intened
    And Removed UGLY spaces from user.ts

--- src/commands/xp.ts
+++ src/commands/xp.ts
@@ -1,49 +1,7 @@
-// import { Command } from "@/command";
-// import {
-//     SlashCommandBuilder,
-//     AutocompleteInteraction,
-//     type Client,
-//     type CommandInteraction,
-// } from "discord.js";
-
-// export default class XpCommand extends Command {
-//     override get info(): any {
-//         return new SlashCommandBuilder()
-//             .setName("xp")
-//             .setDescription("Everything to do with XP!")
-//             .addUserOption((option) => 
-//                 option.setName("user").setDescription("User")
-//             )
-//             // .addStringOption((option) =>
-//             //     option.setName("option").setDescription("Option").setAutocomplete(true).setRequired(true),
-//             // )
-//             .toJSON();
-//     }
-
-
-//     // override async executeAutoComplete(client: Client, interaction: AutocompleteInteraction): Promise<void> {
-// 	// 	const focusedOption = interaction.options.getFocused();
-// 	// 	const choices = ['Popular Topics: Threads', 'Sharding: Getting started', 'Library: Voice Connections', 'Interactions: Replying to slash commands', 'Popular Topics: Embed preview'];
-// 	// 	const filtered = choices.filter(choice => choice.startsWith(focusedOption));
-// 	// 	await interaction.respond(
-// 	// 		filtered.map(choice => ({ name: choice, value: choice })),
-// 	// 	);
-//     // }
-
-//     override async executeCommand(client: Client, interaction: CommandInteraction<any>): Promise<void> {
-//         let member = interaction.options.get("user")?.member;
-//         // let option = interaction.options.get("option")?.value;
-
-//         console.log(member);
-
-//         interaction.reply("Pong Works! " + member?.nickname || "nope");
-//     }
-// }
-
 import { Command } from "@/command";
 import { SlashCommandBuilder, User, type Client, type CommandInteraction } from "discord.js";
 import mongoose from "mongoose";
-import { UserModel } from '../models/user';
+import { giveXP, UserModel } from '../models/user';
 
 export default class XpCommand extends Command {
     override get info(): any {
@@ -55,12 +13,19 @@ export default class XpCommand extends Command {
                 .setDescription("User")
                 .setRequired(false)
             )
+            .addStringOption((option) =>
+                option.setName("command")
+                .setDescription("The command you want to perform to the user")
+                .setRequired(false)
+            )
             .toJSON();
     }
 
     override async executeCommand(client: Client, interaction: CommandInteraction<any>): Promise<void> {
         const userOption = interaction.options.get("user", false);
         const user = userOption?.user ?? interaction.user;
+
+        let message: string;
 
         let xp: number | undefined;
 
@@ -71,7 +36,33 @@ export default class XpCommand extends Command {
                 break;
             }
         }
+        message = `${user} has ${xp || "no"} xp`;
 
-        interaction.reply(`${user.username} has ${xp || "no"} xp`);
+        // Command stuff
+        const commandOption = interaction.options.get("command", false);
+        if (commandOption != null) { // Needs to be checked for admin
+            const command: string[] = commandOption.value?.toString().split(" ") || [];
+            switch (command[0]) {
+                case "grant":
+                    const xp = Number(command[1]);
+                    giveXP(user.id, xp);
+                    message = `${interaction.user} granted ${user} ${xp} xp`;
+                    break;
+
+                default:
+                    message = `Command not found: "${commandOption.value as string}" ¯\\_(ツ)_/¯`;
+                    break;
+            }
+
+            // if (command[0] == "grant") {
+            //     const xp = Number(command[1]);
+            //     giveXP(user.id, xp);
+            //     message = `${interaction.user} granted ${user} ${xp} xp`;
+            // } else  {
+            //     message = `Command not found: "${commandOption.value as string}" ¯\_(ツ)_/¯`;
+            // }
+        }
+
+        interaction.reply(message);
     }
 }
