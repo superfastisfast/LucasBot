42c5b42e7032403c9ea73099a2b8dc651df52c8f
Lucas <luckeeklund@gmail.com>
2025-06-16 20:06:24 +0300

    patch

--- src/quests/unstablePortal.ts
+++ src/quests/unstablePortal.ts
@@ -72,7 +72,7 @@ export default class UnstablePortalQuest extends Quest.Base {
             .setImage(
                 "https://cdn.discordapp.com/attachments/1379101132743250082/1382031141577425076/WEQ4VWpwSE5RPQ.png?ex=6849ac4d&is=68485acd&hm=4cc8f7af4c76a4fd083f4eafb50935fc5e07dec05be3c742b0c25336f33aee8f&",
             )
-            .setURL(Quest.link);
+            .setURL(Globals.LINK);
 
         await this.message.edit({
             embeds: [embed],
